86cef0c05565d48671ccc551eb36aaeac6bf2d44
Harshavardini K <74281712+harshavardini-2003@users.noreply.github.com>
2026-02-01 22:01:58 +0530

    Delete src directory

--- src/src/summarizer.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def summarize_text(text):
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    summary = summarizer(text, max_length=150, min_length=50, do_sample=False)
-    return summary[0]["summary_text"]
